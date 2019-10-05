947e7105175dcf68706c22463bf466c2f19e3d82
aejiade <ejiadedeji@gmail.com>
2019-10-05 18:51:14 -0500

    changes

--- app/src/main/java/com/example/myhealth/MainActivity.java
+++ app/src/main/java/com/example/myhealth/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.myhealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+
+
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    EditText txtDateOfBirth;
+    Button btnDateOfBirth;
+    Calendar c;
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner genderSpinner = findViewById(R.id.genderSpinner);
+        txtDateOfBirth = findViewById(R.id.txtDateOfBirth);
+        btnDateOfBirth = findViewById(R.id.btnDateOfBirth);
+        btnNext = findViewById(R.id.btnNext);
+
+        Spinner genderSpinner = findViewById(R.id.spnGender);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>
                 (MainActivity.this, android.R.layout.simple_list_item_1, getResources()
                         .getStringArray(R.array.genders));
@@ -23,5 +40,40 @@ public class MainActivity extends AppCompatActivity {
         genderSpinner.setAdapter(myAdapter);
 
 
+        btnDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                DatePickerDialog dpd = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDay) {
+                      txtDateOfBirth.setText(mMonth + "/" + mDay + "/" + mYear);
+                    }
+                }, year, month, day);
+                dpd.show();
+            }
+        });
+
+
+//        btnNext.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MainActivity.
+//            }
+//        });
+
+
+
+
+
+
+
+
     }
+
+
 }
